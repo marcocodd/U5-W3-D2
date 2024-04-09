19a9e80ec4259d195eeedcffa8ba018568899b15
Marco Brunetti <151136910+marcocodd@users.noreply.github.com>
2024-04-09 17:14:15 +0200

    GetMapping Autori

--- src/main/java/marco/U5W3D2/controllers/AutoreController.java
+++ src/main/java/marco/U5W3D2/controllers/AutoreController.java
@@ -1,10 +1,23 @@
 package marco.U5W3D2.controllers;
 
+import marco.U5W3D2.entities.Autore;
+import marco.U5W3D2.services.AutoriService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/autori")
 public class AutoreController {
+    @Autowired
+    private AutoriService autoriService;
+
+    @GetMapping
+    private List<Autore> getAllBlogs() {
+        return this.autoriService.getAutoriList();
+    }
 
 }
--- src/main/java/marco/U5W3D2/services/AutoriService.java
+++ src/main/java/marco/U5W3D2/services/AutoriService.java
@@ -13,5 +13,6 @@ public class AutoriService {
     public List<Autore> getAutoriList() {
         return autoriList;
     }
+
     
 }
