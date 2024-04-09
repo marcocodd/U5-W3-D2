1d7837e1a219fe8bc36e3e1cf482722a4e1eaa15
Marco Brunetti <151136910+marcocodd@users.noreply.github.com>
2024-04-09 15:49:50 +0200

    creata struttura

---
+++ src/main/java/marco/U5W3D2/controllers/AutoreController.java
@@ -0,0 +1,10 @@
+package marco.U5W3D2.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/autori")
+public class AutoreController {
+
+}
---
+++ src/main/java/marco/U5W3D2/entities/Blog.java
@@ -0,0 +1,79 @@
+package marco.U5W3D2.entities;
+
+public class Blog {
+
+    private int id;
+    private String categoria;
+    private String titolo;
+    private String cover;
+    private String contenuto;
+    private int tempoDiLettura;
+
+    public Blog() {
+    }
+
+    public Blog(int id, String categoria, String titolo, String cover, String contenuto, int tempoDiLettura) {
+        this.id = id;
+        this.categoria = categoria;
+        this.titolo = titolo;
+        this.cover = cover;
+        this.contenuto = contenuto;
+        this.tempoDiLettura = tempoDiLettura;
+    }
+
+    @Override
+    public String toString() {
+        return "Blog{" +
+                "id=" + id +
+                ", categoria='" + categoria + '\'' +
+                ", titolo='" + titolo + '\'' +
+                ", cover='" + cover + '\'' +
+                ", contenuto='" + contenuto + '\'' +
+                ", tempoDiLettura=" + tempoDiLettura +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+
+    public String getCover() {
+        return cover;
+    }
+
+    public void setCover(String cover) {
+        this.cover = cover;
+    }
+
+    public String getContenuto() {
+        return contenuto;
+    }
+
+    public void setContenuto(String contenuto) {
+        this.contenuto = contenuto;
+    }
+
+    public int getTempoDiLettura() {
+        return tempoDiLettura;
+    }
+
+    public void setTempoDiLettura(int tempoDiLettura) {
+        this.tempoDiLettura = tempoDiLettura;
+    }
+}
